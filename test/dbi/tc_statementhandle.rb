c42c0e13e3ba43fe86d5579edf285ba6cb855a8b
pilcrow <mjp@pilcrow.madison.wi.us>
2009-09-27 22:28:44 -0500

    in progress / scratch commit

--- test/dbi/tc_statementhandle.rb
+++ test/dbi/tc_statementhandle.rb
@@ -2,6 +2,81 @@ $: << 'lib'
 require 'test/unit'
 require 'dbi'
 
+module MandatorySthTests
+  @@oops = ::NotImplementedError.new("Developer forgot to write test!")
+
+  def test_fetchable;   raise @@oops end
+  def test_finished;    raise @@oops end
+  def test_fetch;       raise @@oops end
+  def test_cancel;      raise @@oops end
+  def test_execute;     raise @@oops end
+  def test_finish;      raise @@oops end
+  def test_bind_param;  raise @@oops end # XXX - DBI should enforce count...
+  def test_column_info; raise @@oops end
+
+  private
+  def me
+    self.class.name.sub(/^Test/, '')
+  end
+end
+
+module SthBehaviorUnexecuted
+  def test_fetchable
+    assert !@sth.fetchable?, "#{me} is not fetchable"
+  end
+  def test_finished
+    assert !@sth.finished?, "#{me} is not #finish()d"
+  end
+  def test_cancel
+    assert_nothing_raised("#{me} may be harmlessly #cancel()d") do
+      @sth.cancel
+    end
+  end
+  def test_finish
+    assert_nothing_raised("#{me} may be #finish()d") do
+      @sth.finish
+    end
+    assert @sth.finished
+  end
+  def test_execute
+    2.times {
+      assert_nothing_raised("#{me} may be executed") do
+        @sth.execute
+      end
+    }
+  end
+  def test_column_info
+    assert_raises(DBI::InterfaceError) do
+    end
+  end
+end
+
+class TestSthInitialized; end
+class TestSthCanceled; end
+class TestSthExecuted; end
+class TestSthExecutedUnbound; end
+class TestSthPartiallyFetched; end
+class TestSthExhaustedSilent; end
+class TestSthExhaustedError; end
+class TestSthExecuted; end
+class TestSthFinished; end
+
+class TestSthUnexecuted < Test::Unit::TestCase
+  include MandatorySthTests
+
+  def test_finished
+    assert !@sth.finished
+  end
+
+  def test_bind_param
+    assert_raises(DBI::InterfaceError) do
+      @sth.bind_param(1, "Billy-Mack")
+    end
+  end
+
+  def test
+end
+
 class MockDriverSth
     # def bind_param; end  # not quite yet
 
